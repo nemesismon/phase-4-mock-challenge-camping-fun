33187e9d787ecc79c3d803c7779509c8407ec4f1
Michael <michael.w.kolb@gmail.com>
2022-11-01 13:42:34 -0400

    commit tree 4f3e69bc06ba238c661d288e430536463dd55bdb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :campers, only: [:create, :index, :show] do
+    resources :activities, only: [:index, :show]
+  end
+
+  resources :activities, only: [:index, :destroy] do
+    resources :signups, only: [:show, :destroy]
+  end
+
+  resources :signups, only: [:create] do
+    resources :activities, only: [:index, :show, :create]
+  end
+
 end
